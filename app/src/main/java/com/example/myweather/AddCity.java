5e366cb51375f95f2ac4eef3dd83790d62b53b0f
alexandre.brancolini <alexandre.brancolini@laposte.net>
2020-11-11 10:45:49 +0100

    ADD AlertDialog

--- app/src/main/java/com/example/myweather/AddCity.java
+++ app/src/main/java/com/example/myweather/AddCity.java
@@ -1,11 +1,14 @@
 package com.example.myweather;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddCity extends AppCompatActivity  {
@@ -19,20 +22,32 @@ public class AddCity extends AppCompatActivity  {
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        //We add the "menu_order" to the current activity
         getMenuInflater().inflate(R.menu.menu_add_city, menu);
         return super.onCreateOptionsMenu(menu);
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item)
     {
-        //We handle the click on a menu item
-        if (item.getItemId() == R.id.profile)
+        if (item.getItemId() == R.id.trash)
         {
-            Intent profile = new Intent(this, AddCity.class);
-            startActivity(profile);
+            new AlertDialog.Builder(this)
+                    .setTitle("WARNINGS!")
+                    .setMessage("Are you sure you want to delete this city?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Continue with delete operation
+                            Toast.makeText(getApplicationContext(), "Weather for the city called Paris deleted", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
-
 
         return super.onOptionsItemSelected(item);
     }
