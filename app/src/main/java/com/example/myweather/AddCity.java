6672b491ee9fa4a3db320c4d96157876c0e6161f
alexandre.brancolini <alexandre.brancolini@laposte.net>
2020-11-11 10:17:22 +0100

    Add menu

--- app/src/main/java/com/example/myweather/AddCity.java
+++ app/src/main/java/com/example/myweather/AddCity.java
@@ -1,6 +1,11 @@
 package com.example.myweather;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddCity extends AppCompatActivity  {
@@ -10,4 +15,27 @@ public class AddCity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_city);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        //We add the "menu_order" to the current activity
+        getMenuInflater().inflate(R.menu.menu_add_city, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        //We handle the click on a menu item
+        if (item.getItemId() == R.id.profile)
+        {
+            Intent profile = new Intent(this, AddCity.class);
+            startActivity(profile);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
